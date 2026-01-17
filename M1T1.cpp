2c72928d1dd0787033c3223614711e49fccd8c7d
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-01-17 15:19:29 +0000

    Finished M1T1

--- M1T1.cpp
+++ M1T1.cpp
@@ -10,5 +10,12 @@ Rodrigua4692
 using namespace std;
 
 int main() {
+    //Greet the user
+    cout <<"Hello, user" <<endl; // This is the "endline"
+    cout << "What's your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
+
     return 0;
 }
